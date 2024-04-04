75995b5bc0d178120d639e0f9788cb0253d286b3
Lucy-cao <caolu8815@163.com>
2024-04-04 18:13:15 +0800

    泛型：通配符

--- 09-Generic/WildcardDemo.java
+++ 09-Generic/WildcardDemo.java
@@ -3,11 +3,18 @@ import java.util.List;
 
 public class WildcardDemo {
     public static void main(String[] args) {
+        //1.有界通配符
         List<Integer> li = Arrays.asList(1, 2, 5);
         System.out.println("sum = " + upperBoundedWildCard(li));
 
         List<Double> ld = Arrays.asList(1.4, 3.5, 2.8);
         System.out.println("sum = " + upperBoundedWildCard(ld));
+
+        //2.无界通配符
+        //下面这行会报错：List<Integer>不是List<Object>的子类
+//        unBoundedWildcard1(li);
+        unBoundedWildcard2(li);
+        unBoundedWildcard2(ld);
     }
 
     public static double upperBoundedWildCard(List<? extends Number> list) {
@@ -17,5 +24,17 @@ public class WildcardDemo {
             s += n.doubleValue();
         }
         return s;
+    }
+
+    public static void unBoundedWildcard1(List<Object> list) {
+        for (Object elm : list) {
+            System.out.println(elm + " ");
+        }
+    }
+
+    public static void unBoundedWildcard2(List<?> list) {
+        for (Object elm : list) {
+            System.out.println(elm + " ");
+        }
     }
 }
